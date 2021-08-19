2d2757d47f62f30ccc2daed9d05868b62c2fcedd
nikorenos <66128921+nikorenos@users.noreply.github.com>
2021-08-19 18:41:22 +0200

    builds jar app

--- src/main/java/com/creativelabs/scriptscreator/ScriptsCreatorApp.java
+++ src/main/java/com/creativelabs/scriptscreator/ScriptsCreatorApp.java
@@ -6,15 +6,19 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ScriptsCreatorApp extends SpringBootServletInitializer {
+public class ScriptsCreatorApp /*extends SpringBootServletInitializer*/ {
 
 	public static void main(String[] args) {
+		SpringApplication.run(ScriptsCreatorApp.class, args);
+	}
+
+	/*public static void main(String[] args) {
 		SpringApplication.run(ScriptsCreatorApp.class, args);
 	}
 
 	@Override
 	protected SpringApplicationBuilder configure (SpringApplicationBuilder application) {
 		return application.sources(ScriptsCreatorApp.class);
-	}
+	}*/
 
 }
