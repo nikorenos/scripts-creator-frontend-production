469fdef966c87f3af0d37a9127ae24d33fac39fd
Sentarion <mbalabox@gmail.com>
2025-01-11 21:27:40 +0100

    add deep l glossary

--- src/main/java/com/creativelabs/scriptscreator/translator/DeepLTranslator.java
+++ src/main/java/com/creativelabs/scriptscreator/translator/DeepLTranslator.java
@@ -1,6 +1,7 @@
 package com.creativelabs.scriptscreator.translator;
 
 
+import com.deepl.api.TextTranslationOptions;
 import com.deepl.api.Translator;
 
 class DeepLTranslator {
@@ -8,9 +9,10 @@ class DeepLTranslator {
     public static String translateDeepL(String text, String sourceLanguage, String targetLanguage) throws Exception {
         //TODO remove from here
         String authKey = "token";
+        TextTranslationOptions options = new TextTranslationOptions().setGlossaryId("id");
         Translator translator;
         translator = new Translator(authKey);
-        return translator.translateText(text, sourceLanguage, targetLanguage).getText();
+        return translator.translateText(text, sourceLanguage, targetLanguage, options).getText();
     }
 
     public static void main(String[] args) throws Exception {
